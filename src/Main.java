964256a33e44ec3ce4bd2ae9586657787ad22027
AdamShi <adams221@nycstudents.net>
2025-10-14 12:35:09 -0400

    new

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+     WordMatch w = new WordMatch("concatenation");
+     System.out.println(w.scoreGuess("ten"));
+     System.out.println(w.scoreGuess("nation"));
+     System.out.println(w.scoreGuess("ten", "nation");
+     System.out.println(w.scoreGuess("con"));
+     System.out.println(w.scoreGuess("cat"));
+     System.out.println(w.scoreGuess("con", "cat"));
     }
 }
--- src/WordMatch.java
+++ src/WordMatch.java
@@ -2,7 +2,7 @@ public class WordMatch {
     private String secret;
 
     public WordMatch(String word){
-
+        secret = word;
     }
 
     public int scoreGuess(String guess){
@@ -11,12 +11,17 @@ public class WordMatch {
             if (secret.substring(i, i + guess.length()).equals(guess)) {
                 score++;
             }
-            return 0;
         }
         return score * guess.length() * guess.length();
     }
 
-    public String findBetterGuess(String guess1, String guess2){
-        return null;
+    public String findBetterGuess(String guess1, String guess2) {
+        if (scoreGuess(guess1)> scoreGuess(guess2)){
+            return guess1;
+        }
+        if (scoreGuess(guess2)> 0) {
+            return guess1;
+        }
+        return guess2;
     }
 }
