8cab1b6c1392fd1e880ca27f9435968e99c5eb20
AdamShi <adams221@nycstudents.net>
2025-10-17 12:16:12 -0400

    new

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-     WordMatch w = new WordMatch("concatenation");
-     System.out.println(w.scoreGuess("ten"));
-     System.out.println(w.scoreGuess("nation"));
-     System.out.println(w.scoreGuess("ten", "nation");
-     System.out.println(w.scoreGuess("con"));
-     System.out.println(w.scoreGuess("cat"));
-     System.out.println(w.scoreGuess("con", "cat"));
+     WordMatch game = new WordMatch("concentration");
+     System.out.println(game.scoreGuess("cen"));
+     System.out.println(game.scoreGuess("tration"));
+     System.out.println(game.findBetterGuess("tration", "cen"));
+     System.out.println(game.scoreGuess("con"));
+     System.out.println(game.scoreGuess("cen"));
+     System.out.println(game.findBetterGuess("con", "con"));
     }
 }
